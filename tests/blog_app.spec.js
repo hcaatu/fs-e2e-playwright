e017eda1a14b3eb16fff0dddd2bc0ac28ddee9f9
hcaatu <aatu.hokkane@gmail.com>
2025-02-19 16:49:54 +0200

    5.20 done

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,9 +1,10 @@
 const { test, describe, beforeEach, expect } = require('@playwright/test')
+const { loginWith, createBlog } = require('./utility')
 
 describe('blog app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('http://localhost:3003/api/testing/reset')
-    await request.post('http://localhost:3003/api/users', {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
       data: {
         name: 'Super user',
         username: 'admin',
@@ -11,7 +12,7 @@ describe('blog app', () => {
       }
     })
 
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
@@ -22,25 +23,34 @@ describe('blog app', () => {
   })
 
   describe('Login', () => {
-    // const usernameBox = await page.getByRole('textbox', { name: 'username' })
-    // const passwordBox = await page.getByRole('textbox', { name: 'password' })
-    // const loginButton = await page.getByRole('button', { name: 'login' })
-
     test('is successful with correct credentials', async ({ page }) => {
-      await page.getByRole('textbox', { name: 'username' }).fill('admin')
-      await page.getByRole('textbox', { name: 'password' }).fill('141414')
-      await page.getByRole('button', { name: 'login' }).click()
-  
+      await loginWith(page, 'admin', '141414')
       await expect(page.getByText('Super user logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByRole('textbox', { name: 'username' }).fill('not admin')
-      await page.getByRole('textbox', { name: 'password' }).fill('252525')
-      await page.getByRole('button', { name: 'login' }).click()
-
+      await loginWith(page, 'not admin', '252525')
       await expect(page.getByText('Wrong username or password')).toBeVisible()
       await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach( async ({ page }) => {
+      await loginWith(page, 'admin', '141414')
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(page, 'Type wars', 'Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html')
+      await expect(page.getByText('created a new blog titled Type wars')).toBeVisible()
+      await expect(page.getByTestId('Type wars')).toBeVisible()
+    })
+
+    test('a blog can be liked', async ({ page }) => {
+      await createBlog(page, 'Type wars', 'Robert C. Martin', 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html')
+      await page.getByRole('button', { name: 'view' }).click()
+      await page.getByRole('button', { name: 'like' }).click()
+      await expect(page.getByText('likes 1')).toBeVisible()
     })
   })
 })
